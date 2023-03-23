4766eec30a8e46874e4e5e4a363b70924f5be4e3
Muhammad Asim <chasim1982@gmail.com>
2023-03-23 16:47:45 +0500

    terraform eks secrets with secret manager

---
+++ terraform/eks-secrets/README.md
@@ -0,0 +1,15 @@
+# AWS EKS Secrets
+
+- step 1 create a eks service account with terraform
+
+- Step2 Install CSI Secret Store Provider for AWS https://github.com/aws/secrets-store-csi-driver-provider-aws
+
+```helm
+helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm install -n kube-system csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
+```
+
+- Step 3 Install the aws provider
+```kubectl
+kubectl apply -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml
+```
---
+++ terraform/eks-secrets/terraform/secret-service-account.tf
@@ -0,0 +1,49 @@
+# Aws Secret Manager Access Policy
+  module "secret_manager_access_policy" {
+    source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+    version = "5.10.0"
+
+    create_policy = true
+    description   = "Allow aws Secrets Manager access"
+    name          = "eks-secret-manager-policy"
+    path          = "/"
+    policy        = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Sid": "AllowSecretsManagerAccess",
+        "Effect": "Allow",
+        "Action": [
+            "secretsmanager:GetResourcePolicy",
+            "secretsmanager:GetSecretValue",
+            "secretsmanager:DescribeSecret",
+            "secretsmanager:ListSecretVersionIds"
+        ],
+        "Resource": "*"
+        }
+    ]
+}
+
+POLICY
+
+    tags = {
+      Environment = "dev"
+      Terraform   = "true"
+    }
+
+  }
+
+# Create a role that can be assumed by our service account
+module "iam-assumable-role-with-oidc-just-like-iam-role-attachment-to-ec2" {
+    source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+    version = "5.10.0"
+
+    create_role      = true
+    role_name        = "eks-secret-manager-role"
+    provider_url     = module.eks.cluster_oidc_issuer_url
+    role_policy_arns = [
+      module.secret_manager_access_policy.arn
+    ]
+
+  }

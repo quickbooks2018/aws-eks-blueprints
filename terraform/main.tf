8fbdc3a16b493f32a616761cbd4f5585138270a8
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2023-09-17 10:50:17 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -561,6 +561,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.default.token
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
